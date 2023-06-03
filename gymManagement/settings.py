b5d5536eb18f5e2f144d3ad8696bae4a215f91e2
Paras Saxena <85274192+parassaxena06@users.noreply.github.com>
2023-06-03 18:38:42 +0530

    Update settings.py

--- gymManagement/settings.py
+++ gymManagement/settings.py
@@ -10,9 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
-import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
